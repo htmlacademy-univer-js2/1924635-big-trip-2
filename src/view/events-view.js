52f32ed230976a834336e61c68e701151706bc3e
NikiforovRoman <romannikiforov86@gmail.com>
2023-06-03 00:24:42 +0500

    module2-task1 fix 1.1

--- src/view/events-view.js
+++ src/view/events-view.js
@@ -1,4 +1,4 @@
-import { createElement } from "../render";
+import { createElement } from '../render';
 
 const eventTemplate = () => (`<ul class="trip-events__list"></ul>`)
 
@@ -6,14 +6,14 @@ export default class EventsView {
   getTemplate() {
     return eventTemplate;
   }
-    
+
   getElement() {
     if (!this.element) {
       this.element = createElement(this.getTemplate());
     }
     return this.element;
   }
-    
+
   removeElement() {
     this.element = null;
   }
