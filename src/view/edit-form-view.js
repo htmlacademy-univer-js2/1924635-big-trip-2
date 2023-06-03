339a87ea86ab93770351842909c50c5fffa2430d
NikiforovRoman <romannikiforov86@gmail.com>
2023-06-03 09:45:28 +0500

    module3-task1 fix 1

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -1,5 +1,5 @@
-import { createElement } from "../render";
-import { getDateTime } from "../utils";
+import { createElement } from '../render';
+import { getDateTime } from '../utils';
 
 const renderDestinationPictures = (pictures) => {
   let result = '';
@@ -146,14 +146,14 @@ export default class EditFormView {
   get template() {
     return editFormTemplate(this.#point, this.#destinations, this.#offers);
   }
-    
+
   get element() {
     if (!this.#element) {
       this.#element = createElement(this.template);
     }
     return this.#element;
   }
-    
+
   removeElement() {
     this.#element = null;
   }
