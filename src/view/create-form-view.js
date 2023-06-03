2ee465ac3d0e71f84b20b3f9958e92c3a5acf98c
NikiforovRoman <romannikiforov86@gmail.com>
2023-06-03 09:47:47 +0500

    module3-task1 fix 1.1

--- src/view/create-form-view.js
+++ src/view/create-form-view.js
@@ -143,20 +143,20 @@ const createFormTemplate = () => (`<li class="trip-events__item">
 </li>`);
 
 export default class CreateFormView {
-  #element = null
+  #element = null;
 
   get template() {
     return createFormTemplate();
   }
-  
+
   get element() {
     if (!this.#element) {
       this.#element = createElement(this.template);
     }
     return this.#element;
   }
-  
+
   removeElement() {
     this.#element = null;
   }
-} 
+}
