64863f42555829a5ed2e7ac4632104853df0ff9b
NikiforovRoman <romannikiforov86@gmail.com>
2023-06-04 16:30:20 +0500

    module4-task1 fix 1.1

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,7 +2,7 @@ const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   return Math.floor(lower + Math.random() * (upper - lower + 1));
-  };
+};
 
 const getRandomElement = (elements) => {
   const MIN = 0;
